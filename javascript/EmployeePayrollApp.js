f6697eda1fe7adcd812a332aa069f3eb6451e3c4
prajaktabramhe <prajakta14bramhe@gmail.com>
2021-04-26 21:38:01 +0530

    [Prajakta]: set event listener on salary range to display appropriate value

---
+++ javascript/EmployeePayrollApp.js
@@ -0,0 +1,8 @@
+//UC8 salary Range
+const salary = document.querySelector('#salary');
+const output = document.querySelector('.salary-output');
+//in range button the output shown should always be equal to value the user is updating
+output.textContent = salary.value;
+salary.addEventListener('input', function () {
+    output.textContent = salary.value;
+});

42645e325c517c8a79e61e30b006006dfc8356b0
prajaktabramhe <prajakta14bramhe@gmail.com>
2021-04-27 11:40:03 +0530

    [Prajakta]: Added: Employee payroll class with new Atrributes

--- javascript/EmployeePayrollData.js
+++ javascript/EmployeePayrollData.js
@@ -8,43 +8,88 @@ class EmployeePayrollData
       this.gender = params[3];
       this.date = params[4];
     }
+ //getter and setter method
+  get id()
+  {
+    return this._id;
+  }
+  set id(id)
+  {
+    this._id = id;
+  }
 
-    get name() 
-    {
+  get name() 
+  {
       return this._name;
-    }
-    set name(name)
-    {
+  }
+  set name(name)
+  {
       let nameRegex = RegExp("^[A-Z]{1}[a-z]{2,}$");
        
-        if (nameRegex.test(name)) 
+      if (nameRegex.test(name)) 
         this._name = name;
-        else throw "Name is incorrect";
-    } 
+      else throw "Name is incorrect";
+  } 
+get profillePic()
+{
+  return this._profilePic;
+}
+set profillePic(profillePic)
+{
+  this._profilePic = profillePic;
+}
+get geneder()
+{
+  return this._gender;
+}
+set gender()
+{
+  this._gender = gender;
+}
+get department()
+{
+  return this._department;
+}
+set department(department)
+{
+this._department = department;
+}
+get salary()
+{
+  return this._salary;
+}
+set salary(salary)
+{
+  this._salary = salary;
+}
 
-    get startDate() 
-    {
-        return this._startDate;
-    }
+get note()
+{
+  return this._note;
+}
+set note(note)
+{
+  this._note = note;
+}
 
-    set startDate(startDate) 
-    {
-    //if it is a future date it will throw error
-        if (startDate > new Date()) // date func will provide todays date
-        throw "Must not be Future Date";
-        else 
-        this._startDate = startDate;
-    }
+get startDate() 
+{
+  return this._startDate;
+}
 
-      //method
-      toString()
-    {
+set startDate(startDate) 
+{
+    this._startDate = startDate;
+}
+
+ //method
+toString()
+  {
         const options = { year: 'numeric', month: 'short', day: 'numeric' };
         //if start date is not in the given datatype it will pass the value undefined to empdate
         const empDate = !this.startDate ? "undefined" :
         this.startDate;
         //here in return for start date we are passing empDate as value                
-        return "id=" + this.id + ", name='" + this.name + ", gender='" + this.gender +
-            ", salary=" + this.salary + ", startDate=" + empDate;
-    }
+        return "id=" + this.id + ", name='" + this.name + ", gender='" + this.gender + ", profilePic=" +  ", department =" + this.department, ",salary=" + this.salary + ", startDate=" + empDate + ", note =" + this.note;
+  }
 }

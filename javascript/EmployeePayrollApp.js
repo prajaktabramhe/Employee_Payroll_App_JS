64be6118b0315e6c578825e7267e542bc3ffb0ef
prajaktabramhe <prajakta14bramhe@gmail.com>
2021-04-29 12:31:42 +0530

    [Prajakta]: Used Reset Function to Reset the form on clicking on reset button

--- javascript/EmployeePayrollApp.js
+++ javascript/EmployeePayrollApp.js
@@ -38,46 +38,94 @@ const save = () => {
         return;
    }
 }
+function createAndUpdateStorage(employeePayrollData)
+{
+    let EmployeePayrollList = JSON.parse(localStorage.getItem("EmployeePayrollList"));
+        if(EmployeePayrollList != undefined)
+            {
+                EmployeePayrollList.push(employeePayrollData);
+            } 
+        else 
+            {
+                EmployeePayrollList = [employeePayrolData]
+                
+            }
+        alert(EmployeePayrollList.toString());
+        localStorage.setItem("EmployeePayrollList", JSON.stringify(EmployeePayrollList));
+}
 //Use Case 13
 
 const createEmployeePayroll = () => {
     
     let employeePayrollData = new EmployeePayrollData();
-    try {
+    try 
+    {
         employeePayrollData.name = getInputValueById('#name');
-    } catch (e) {
+    } 
+    catch (e) 
+    {
         setTextValue('.text-error', e);
         throw e;
     }
-            employeePayrollData.profilePic = getSelectedValues('[name=profile]').pop();
+        employeePayrollData.profilePic = getSelectedValues('[name=profile]').pop();
     
-            employeePayrollData.gender = getSelectedValues('[name=gender]').pop();
-            employeePayrollData.department = getSelectedValues('[name=department]');
-            employeePayrollData.salary = getInputValueById('#salary');
-            employeePayrollData.note = getInputValueById('#notes');
-            let date = getInputValueById('#day') +" "+ getInputValueById('#month') +" "+ getInputValueById('#year');
-            employeePayrollData.date = Date.parse(date);
-            alert(employeePayrollData.toString());
-            return employeePayrollData;
+        employeePayrollData.gender = getSelectedValues('[name=gender]').pop();
+        employeePayrollData.department = getSelectedValues('[name=department]');
+        employeePayrollData.salary = getInputValueById('#salary');
+        employeePayrollData.note = getInputValueById('#notes');
+        let date = getInputValueById('#day') +" "+ getInputValueById('#month') +" "+ getInputValueById('#year');
+        employeePayrollData.date = Date.parse(date);
+        alert(employeePayrollData.toString());
+        return employeePayrollData;
 
-            }
+    }
 
-            const getSelectedValues = (propertyValue) => {
-                console.log(propertyValue);
-            let allItems = document.querySelectorAll(propertyValue);
-            let selItems = [];
-            allItems.forEach(item => {
-                if (item.checked) selItems.push(item.value)
-            });
-            return selItems;
-            }
+    const getSelectedValues = (propertyValue) => {
+    console.log(propertyValue);
+    let allItems = document.querySelectorAll(propertyValue);
+    let selItems = [];
+    allItems.forEach(item => {
+        if (item.checked) selItems.push(item.value)
+    });
+        return selItems;
+    }
 
-            const getInputValueById = (id) => {
-            let value = document.querySelector(id).value;
-            return value;
-            }
-            const getInputElementValue = (id) => {
-                let value = document.getElementById(id).value;
-                return value;
-                }
+    const getInputValueById = (id) => {
+    let value = document.querySelector(id).value;
+    return value;
+    }
+
+    const getInputElementValue = (id) => {
+    let value = document.getElementById(id).value;
+        return value;
+    }
+const resetForm = () => {
+    setValue('#name', '');
+    unsetSelectedValues('[name=profile]');
+    unsetSelectedValues('[name=gender]');
+    unsetSelectedValues('[name=department]');
+    setValue('#salary','');
+    setValue('#notes','');
+    setValue('#day','1');
+    setValue('#month','January');
+    setValue('#year','2020');
+}
+const unsetSelectedValues = (propertyValue) => {
+    let allItems = document.querySelectorAll(propertyValue);
+    allItems.forEach(item => {
+        item.checked = false;
+    });
+}
+
+const setTextValue = (id,value) => {
+    const element = document.querySelector(id);
+    element.textContent = value;
+}
+
+const setValue = (id, value) => {
+    const element = document.querySelector(id);
+    element.value = value;
+}
+
+
 

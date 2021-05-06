b5e5ffbea3c7c891a732906ebf852f7540d32133
prajaktabramhe <prajakta14bramhe@gmail.com>
2021-05-06 20:37:37 +0530

    [Prajakta]: Added: Link for goes to another form

--- javascript/site_properties.js
+++ javascript/site_properties.js
@@ -1,4 +1,4 @@
 let site_properties = {
-    home_page: "../pages/NewEmployeePayrollApp.html",
+    home_page: "../pages/NewEmployeePayrollHome.html",
     add_emp_payroll_page: "../pages/EmployeePayrollApp.html"
 };
